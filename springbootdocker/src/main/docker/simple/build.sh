89abc31ff9335aece4033cc4b0c8a1bf22d1955a
david.rigola <david.frigola@gmail.com>
2016-03-27 10:57:07 +0200

    Fix build for simple docker sample - correct path for cp command

--- springbootdocker/src/main/docker/simple/build.sh
+++ springbootdocker/src/main/docker/simple/build.sh
@@ -1,5 +1,5 @@
 # Copy the fat-jar file to local folder
-cp ../../../target/springbootdocker-0.0.1.jar .
+cp ../../../../target/springbootdocker-0.0.1.jar .
 # Build the image
 docker build -t nms/springboottest .
 
